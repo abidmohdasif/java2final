3f1b06d019a68a32897178746902f74a7873cf5e
Parth Thite <parthpthite@gmail.com>
2026-04-23 09:30:10 -0400

    look up a company by its ticker

--- model/marketSimulator.java
+++ model/marketSimulator.java
@@ -2,3 +2,12 @@ public MarketSimulator() {
     this.companies = MarketDataFactory.createAllCompanies();
     this.currentDay = 1;
 }
+
+public Company findCompany(String ticker) {
+    for (Company c: companies) {
+        if (c.getTicker().equals(ticker)) {
+            return c;
+        }
+    }
+    return null;
+}

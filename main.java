1e476878c002b8f3bc7d7c71abc9f5e53becc915
Parth Thite <parthpthite@gmail.com>
2026-04-27 09:05:55 -0400

    updating main

--- main.java
+++ main.java
@@ -1,1 +1,16 @@
 package com.portfolio.model;
+
+public class Main {
+    public static void main(String[] args) {
+        MarketSimulator market = new MarketSimulator();
+        Player player = new Player(100_000.0);
+        TradeController controller = new TradeController(market, player);
+        MarketView marketView = new MarketView();
+        PortfolioView portfolioView = new PortfolioView();
+
+        marketView.display(market.getCompanies());
+        controller.processBuy("ZENE", 10);
+        controller.advanceDay();
+        portfolioView.display(player, market);
+    }
+}

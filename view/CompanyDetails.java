1049a12524853b4971c13624205569b9c5b1adee
Parth Thite <parthpthite@gmail.com>
2026-04-27 20:05:10 +0000

    Renaming files

--- view/CompanyDetails.java
+++ view/CompanyDetails.java
@@ -1,4 +1,4 @@
-public void displayDetails(Company c) {
+CompanyDetailssplayDetails(Company c) {
     System.out.println("Company: " + c.getName() + " (" + c.getTicker() + ")");
     System.out.println("Price: $" + c.getCurrentPrice());
     System.out.println("About: " + c.getDescription());
